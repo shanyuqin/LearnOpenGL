2dca0e7e7f2bfaccb2d9ee4a938e7ac41ff86500
shanyuqin <>
2020-05-15 19:23:22 +0800

    在观察空间（而不是世界空间）中计算冯氏光照

--- OpenGL08——颜色/OpenGL08——颜色/colors.vs
+++ OpenGL08——颜色/OpenGL08——颜色/colors.vs
@@ -4,15 +4,18 @@ layout (location = 1) in vec3 aNormal;
 
 out vec3 Normal;
 out vec3 FragPos;
+out vec3 LightPos;
 
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
+uniform vec3 lightPos;
 
 void main()
 {
     
-    FragPos = vec3(model * vec4(aPos, 1.0f));
-    Normal = mat3(transpose(inverse(model))) * aNormal;
-    gl_Position = projection * view  * vec4(FragPos, 1.0f);
+    FragPos = vec3(view * model * vec4(aPos, 1.0f));
+    Normal = mat3(transpose(inverse(view * model))) * aNormal;
+    gl_Position = projection  * vec4(FragPos, 1.0f);
+    LightPos = vec3(view * vec4(lightPos, 1.0));
 }
--- OpenGL08——颜色/OpenGL08——颜色/main.cpp
+++ OpenGL08——颜色/OpenGL08——颜色/main.cpp
@@ -140,14 +140,14 @@ int main()
         glad_glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
         //练习1
-        lightPos.x = 1.0f + sin(glfwGetTime()) * 2.0f;
-        lightPos.y = sin(glfwGetTime() / 2.0f) * 1.0f;
+//        lightPos.x = 1.0f + sin(glfwGetTime()) * 2.0f;
+//        lightPos.y = sin(glfwGetTime() / 2.0f) * 1.0f;
         //渲染逻辑 start ——————————————————————————————————————————
         lightingShader.use();
         lightingShader.setVec3("objectColor", 1.0f,0.5f,0.31f);
         lightingShader.setVec3("lightColor", 1.0f,1.0f,1.0f);
         lightingShader.setVec3("lightPos", lightPos);
-        lightingShader.setVec3("viewPos", camera.Position);
+//        lightingShader.setVec3("viewPos", camera.Position);
         
         glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH/(float)SCR_HEIGHT, 0.1f, 100.f);
         glm::mat4 view = camera.GetViewMatrix();
