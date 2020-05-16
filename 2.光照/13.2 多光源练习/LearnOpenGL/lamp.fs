a31935407044dfb9c5ef098813e8794b772afd98
shanyuqin <>
2020-05-16 22:27:29 +0800

    多光源——练习

--- 2.光照/13.2 多光源练习/LearnOpenGL/lamp.fs
+++ 2.光照/13.2 多光源练习/LearnOpenGL/lamp.fs
@@ -4,6 +4,6 @@ out vec4 FragColor;
 uniform vec3 lampColor;
 void main()
 {
-//    FragColor = vec4(lampColor,1.0); //光源颜色
-    FragColor = vec4(1.0); //白色光源
+    FragColor = vec4(lampColor,1.0); //光源颜色
+//    FragColor = vec4(1.0); //白色光源
 }
--- 2.光照/13.2 多光源练习/LearnOpenGL/main.cpp
+++ 2.光照/13.2 多光源练习/LearnOpenGL/main.cpp
@@ -123,6 +123,13 @@ int main()
         glm::vec3(-4.0f,  2.0f, -12.0f),
         glm::vec3( 0.0f,  0.0f, -3.0f)
     };
+    
+    glm::vec3 pointLightColors[] = {
+        glm::vec3(0.4f, 0.7f, 0.1f),
+        glm::vec3(0.4f, 0.7f, 0.1f),
+        glm::vec3(0.4f, 0.7f, 0.1f),
+        glm::vec3(0.4f, 0.7f, 0.1f)
+    };
     unsigned int VBO, cubeVAO , lightVAO;
     glad_glGenVertexArrays(1,&cubeVAO);
     glad_glGenBuffers(1,&VBO);
@@ -165,6 +172,7 @@ int main()
         //      键盘输入
         processInput(window);
         
+        
         glad_glClearColor(0.9f, 0.9f, 0.9f, 1.0f);
         glad_glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
@@ -178,38 +186,38 @@ int main()
         //定向光的属性
         lightingShader.setVec3("dirLight.direction", -0.2f, -1.0f, -0.3f);
         lightingShader.setVec3("dirLight.ambient", 0.05f, 0.05f, 0.05f);
-        lightingShader.setVec3("dirLight.diffuse", 0.4f, 0.4f, 0.4f);
-        lightingShader.setVec3("dirLight.specular", 0.5f, 0.5f, 0.5f);
+        lightingShader.setVec3("dirLight.diffuse", 1.0f, 1.0f, 1.0f);
+        lightingShader.setVec3("dirLight.specular", 1.0f, 1.0f, 1.0f);
         
         // 点光源1的属性
         lightingShader.setVec3("pointLights[0].position", pointLightPositions[0]);
-        lightingShader.setVec3("pointLights[0].ambient", 0.05f, 0.05f, 0.05f);
-        lightingShader.setVec3("pointLights[0].diffuse", 0.8f, 0.8f, 0.8f);
-        lightingShader.setVec3("pointLights[0].specular", 1.0f, 1.0f, 1.0f);
+        lightingShader.setVec3("pointLights[0].ambient", pointLightColors[0].x * 0.1,  pointLightColors[0].y * 0.1,  pointLightColors[0].z * 0.1);
+        lightingShader.setVec3("pointLights[0].diffuse", pointLightColors[0]);
+        lightingShader.setVec3("pointLights[0].specular", pointLightColors[0]);
         lightingShader.setFloat("pointLights[0].constant", 1.0f);
         lightingShader.setFloat("pointLights[0].linear", 0.09);
         lightingShader.setFloat("pointLights[0].quadratic", 0.032);
         // 点光源2的属性
         lightingShader.setVec3("pointLights[1].position", pointLightPositions[1]);
-        lightingShader.setVec3("pointLights[1].ambient", 0.05f, 0.05f, 0.05f);
-        lightingShader.setVec3("pointLights[1].diffuse", 0.8f, 0.8f, 0.8f);
-        lightingShader.setVec3("pointLights[1].specular", 1.0f, 1.0f, 1.0f);
+        lightingShader.setVec3("pointLights[1].ambient", pointLightColors[1].x * 0.1,  pointLightColors[1].y * 0.1,  pointLightColors[1].z * 0.1);
+        lightingShader.setVec3("pointLights[1].diffuse", pointLightColors[1]);
+        lightingShader.setVec3("pointLights[1].specular", pointLightColors[1]);
         lightingShader.setFloat("pointLights[1].constant", 1.0f);
         lightingShader.setFloat("pointLights[1].linear", 0.09);
         lightingShader.setFloat("pointLights[1].quadratic", 0.032);
         // 点光源3的属性
         lightingShader.setVec3("pointLights[2].position", pointLightPositions[2]);
-        lightingShader.setVec3("pointLights[2].ambient", 0.05f, 0.05f, 0.05f);
-        lightingShader.setVec3("pointLights[2].diffuse", 0.8f, 0.8f, 0.8f);
-        lightingShader.setVec3("pointLights[2].specular", 1.0f, 1.0f, 1.0f);
+        lightingShader.setVec3("pointLights[2].ambient", pointLightColors[2].x * 0.1,  pointLightColors[2].y * 0.1,  pointLightColors[2].z * 0.1);
+        lightingShader.setVec3("pointLights[2].diffuse", pointLightColors[2]);
+        lightingShader.setVec3("pointLights[2].specular", pointLightColors[2]);
         lightingShader.setFloat("pointLights[2].constant", 1.0f);
         lightingShader.setFloat("pointLights[2].linear", 0.09);
         lightingShader.setFloat("pointLights[2].quadratic", 0.032);
         // 点光源4的属性
         lightingShader.setVec3("pointLights[3].position", pointLightPositions[3]);
-        lightingShader.setVec3("pointLights[3].ambient", 0.05f, 0.05f, 0.05f);
-        lightingShader.setVec3("pointLights[3].diffuse", 0.8f, 0.8f, 0.8f);
-        lightingShader.setVec3("pointLights[3].specular", 1.0f, 1.0f, 1.0f);
+        lightingShader.setVec3("pointLights[3].ambient", pointLightColors[3].x * 0.1,  pointLightColors[3].y * 0.1,  pointLightColors[3].z * 0.1);
+        lightingShader.setVec3("pointLights[3].diffuse", pointLightColors[3]);
+        lightingShader.setVec3("pointLights[3].specular", pointLightColors[3]);
         lightingShader.setFloat("pointLights[3].constant", 1.0f);
         lightingShader.setFloat("pointLights[3].linear", 0.09);
         lightingShader.setFloat("pointLights[3].quadratic", 0.032);
@@ -218,13 +226,13 @@ int main()
         lightingShader.setVec3("spotLight.position", camera.Position);
         lightingShader.setVec3("spotLight.direction", camera.Front);
         lightingShader.setVec3("spotLight.ambient", 0.0f, 0.0f, 0.0f);
-        lightingShader.setVec3("spotLight.diffuse", 1.0f, 1.0f, 1.0f);
-        lightingShader.setVec3("spotLight.specular", 1.0f, 1.0f, 1.0f);
+        lightingShader.setVec3("spotLight.diffuse", 0.0f, 1.0f, 0.0f);
+        lightingShader.setVec3("spotLight.specular", 0.0f, 1.0f, 0.0f);
         lightingShader.setFloat("spotLight.constant", 1.0f);
-        lightingShader.setFloat("spotLight.linear", 0.09);
-        lightingShader.setFloat("spotLight.quadratic", 0.032);
-        lightingShader.setFloat("spotLight.cutOff", glm::cos(glm::radians(12.5f)));
-        lightingShader.setFloat("spotLight.outerCutOff", glm::cos(glm::radians(15.0f)));
+        lightingShader.setFloat("spotLight.linear", 0.07);
+        lightingShader.setFloat("spotLight.quadratic", 0.017);
+        lightingShader.setFloat("spotLight.cutOff", glm::cos(glm::radians(7.0f)));
+        lightingShader.setFloat("spotLight.outerCutOff", glm::cos(glm::radians(10.0f)));
         
         //裁剪空间和观察空间的变换
         glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH/(float)SCR_HEIGHT, 0.1f, 100.f);
@@ -258,6 +266,7 @@ int main()
         lampShader.use();
         lampShader.setMatrix4fv("projection", projection);
         lampShader.setMatrix4fv("view", view);
+        lampShader.setVec3("lampColor", 0.4f, 0.7f, 0.1f);
       
         glad_glBindVertexArray(lightVAO);
         for (int i = 0; i < 4; i++) {
